6062967cce771a08b1919abba3365a8384c3aa99
Rishabh singh <rishabhsingh.block@gmail.com>
2026-07-18 13:10:28 +0530

    useContext theme toggle with dynamic button text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
 import { Link } from "react-router-dom"
+import { ThemeContext } from "../context/ThemeContext";
+import { useContext } from "react";
+
 
 function Navbar() {
+    const { darkmode, toggle } = useContext(ThemeContext)
     return (
         <>
             <Link to="/">Home</Link>
             <Link to="/favourites"> Favourites </Link>
+            <button onClick={toggle}> {darkmode ? 'DarkTheme' : 'LightTheme'}</button>
         </>
     );
 }
--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,2 +1,15 @@
-import { createContext } from "react"
+import { createContext, useState } from "react";
 
+export const ThemeContext = createContext()
+
+export function ThemeProvider({ children }) {
+    const [darkmode, setDarkmode] = useState(false)
+    const toggle = () => setDarkmode(!darkmode)
+    return (
+        <>
+            <ThemeContext.Provider value={{ darkmode, toggle }}>
+                {children}
+            </ThemeContext.Provider>
+        </>
+    )
+}
